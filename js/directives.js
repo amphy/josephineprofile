f59871e33db9619cb03efddb1762129d4b2c96af
heather huynh <amphy@uga.edu>
2014-12-25 01:07:55 -0500

    everything works

--- js/directives.js
+++ js/directives.js
@@ -1,1 +1,8 @@
 'use strict';
+app.directive('holderFix', function () {
+    return {
+        link: function (scope, element, attrs) {
+            Holder.run({ images: element[0], nocss: true });
+        }
+    };
+});
