d909cf7195ea9969c2b4d570684597b11c3452db
heather huynh <amphy@uga.edu>
2014-12-24 21:03:10 -0500

    this worked before so we going back

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ var app = angular.module('dabbble', ['dabbble.controllers', 'dabbble.filters', '
 
 app.config(function ($routeProvider) {
 	$routeProvider
-	.when("#/about",  {controller:"ShotsCtrl", templateUrl: "partials/about.html"})
-	.when("#/contact",  {controller:"ShotsListCtrl", templateUrl: "partials/contact.html"})
-	.when("#/projects",  {controller:"ShotsListCtrl", templateUrl: "partials/projects.html"})
+	.when("/about",  {controller:"ShotsCtrl", templateUrl: "partials/about.html"})
+	.when("/contact",  {controller:"ShotsListCtrl", templateUrl: "partials/contact.html"})
+	.when("/projects",  {controller:"ShotsListCtrl", templateUrl: "partials/projects.html"})
 	.when("/",  {controller:"ShotsCtrl", templateUrl: "partials/home.html"})
 	.otherwise({redirectTo: "/"});
 	
